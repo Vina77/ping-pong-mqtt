c43499f0ecf8a5acb27305a484e815f37a7f9f19
Leandro Rodrigues <leandromqrs@hotmail.com>
2024-06-20 19:58:15 -0300

    refactor: Changed to make one client per connection

--- mqtt_ping_pong.py
+++ mqtt_ping_pong.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import csv
 import time
+import sys
 import random
 
 from datetime import datetime
@@ -8,6 +9,7 @@ from datetime import datetime
 BROKER = "broker.emqx.io"
 PORT = 1883
 TOPIC = "mqtt/test"
+MESSAGE = sys.argv[1]
 
 connection_timestamp = None
 
@@ -23,51 +25,39 @@ def on_connect(client, userdata, flags, rc, properties):
 
 def on_message(client, userdata, msg):
     global connection_timestamp
-    print("Received message: " + msg.topic + " " + str(msg.payload))
-    if msg.payload.decode() == "ping":
+    if msg.payload.decode() == "ping" and MESSAGE != "ping":
         print("Ping received")
         # Calculate the time since the ping was sent
         now = time.time()
-        ping_ms = int((now - connection_timestamp) * 1000)
-        print(f"Ping timestamp: {ping_ms} milisseconds")
+        connection_ms = int((now - connection_timestamp) * 1000)
+        print(f"Ping timestamp: {connection_ms} milisseconds")
         # Write to CSV
         with open('timestamps.csv', mode='a') as file:
             writer = csv.writer(file)
-            writer.writerow([datetime.fromtimestamp(connection_timestamp), datetime.fromtimestamp(now), ping_ms, "ping"])
+            writer.writerow([datetime.fromtimestamp(connection_timestamp), datetime.fromtimestamp(now), connection_ms, "ping"])
             file.close()
-    elif msg.payload.decode() == "pong":
+    elif msg.payload.decode() == "pong" and MESSAGE != "pong":
         print("Pong received")
         # Calculate the time since the pong was sent
         now = time.time()
-        pong_ms = int((now - connection_timestamp) * 1000)
-        print(f"Pong timestamp: {pong_ms} milisseconds")
+        connection_ms = int((now - connection_timestamp) * 1000)
+        print(f"Pong timestamp: {connection_ms} milisseconds")
         # Write to CSV
         with open('timestamps.csv', mode='a') as file:
             writer = csv.writer(file)
-            writer.writerow([datetime.fromtimestamp(connection_timestamp), datetime.fromtimestamp(now), pong_ms, "pong"])
+            writer.writerow([datetime.fromtimestamp(connection_timestamp), datetime.fromtimestamp(now), connection_ms, "pong"])
             file.close()
 
 # Client 1 - Ping
-ping_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-ping_client.on_connect = on_connect
-ping_client.on_message = on_message
-ping_client.connect(BROKER, PORT, 60)  # Replace with your MQTT server address and port
-ping_client.loop_start()
-
-# Client 2 - Receiver
-pong_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
-pong_client.on_connect = on_connect
-pong_client.on_message = on_message
-pong_client.connect(BROKER, PORT, 60)  # Replace with your MQTT server address and port
-pong_client.loop_start()
+mqtt_client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_connect = on_connect
+mqtt_client.on_message = on_message
+mqtt_client.connect(BROKER, PORT, 60)  # Replace with your MQTT server address and port
+mqtt_client.loop_start()
 
 # Keep the script running to listen for messages
 while True:
     time.sleep(1)
-    # Send "ping" message after connection
+    # Send message after connection
     connection_timestamp = time.time()
-    ping_client.publish(TOPIC, "ping")
-
-    # Send "pong" message after connection
-    connection_timestamp = time.time()
-    pong_client.publish(TOPIC, "pong")
+    mqtt_client.publish(TOPIC, MESSAGE)
